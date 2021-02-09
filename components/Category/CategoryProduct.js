c21dcc62b767f81c696c67f30231740477bec39f
Timo <timo@goedgemerkt.nl>
2021-02-09 19:16:35 +0100

    lazyloading

--- components/Category/CategoryProduct.js
+++ components/Category/CategoryProduct.js
@@ -1,11 +1,18 @@
 import styles from './Category.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function CategoryProduct({ data }){
     return(
         <Link href={data.url_key}>
             <div className={styles.CategoryProduct}>
-                <img src={data.small_image.url} alt={data.small_image.label} />
+                <Image
+                    src={data.small_image.url}
+                    alt={data.small_image.label}
+                    width={500}
+                    height={500}
+                />
+                <p>{data.name}</p>
             </div>
         </Link>
     )
